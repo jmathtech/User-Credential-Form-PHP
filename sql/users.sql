adf1e6cea0cefc7d8daabc3a6b84f18e5e6c5406
Jamil Matheny <36749450+jmathtech@users.noreply.github.com>
2020-10-10 22:57:35 -0400

    Update users.sql

--- sql/users.sql
+++ sql/users.sql
@@ -48,20 +48,20 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`Cus_ID`, `Cus_FirstName`, `Cus_LastName`, `Cus_Email`, `Cus_Phone`, `Cus_Address`, `Cus_BusinessName`, `Cus_BusinessPhone`, `Cus_BusinessEmail`, `Cus_BusinessTaxID`, `Cus_NumOfStaff`) VALUES
-(1, 'Ashley', 'Kilson', 'ashley@gmail.com', '8049238982', '5689 Stony Point Dr. Richmond, VA 23225', 'Ashley\'s store', '804723998', 'ashley@ashley.com', '83-9203039', '20'),
-(2, 'Miles', 'Davis', 'davison.comp@yahoo.com', '928393878', '245 Seagate Blvd. Beverly Hills, CA 90219', 'Miles & Co', '938393929', 'miles.davis@davisonco.com', '89-8238492', '1099'),
+(1, 'Ashley', 'Kilson', 'ashley@gmeil.com', '8049238982', '5689 Stone Point Dr. Mondale, VA 23555', 'Bank Parpley\'s store', '804723998', 'ashley@bankparpleys.com', '83-9203039', '20'),
+(2, 'Miles', 'Davis', 'davison.comp@yah00.com', '928393878', '245 Seagate Blvd. Beverly Hills, CA 90219', 'Miles & Co', '938393929', 'miles.davis@davisonco.com', '89-8238492', '1099'),
 (3, 'Dowdle', 'Timothy', 'tdowdle@dreamsfactory.com\r\n', '18043065388', '2923 Fairfield Dr. Chesterfield, VA 23005', 'Dreams Factory, Inc.', '18043065388', 'tdowdle@dreamsfactory.com\r\n', '78-2903842', '3545'),
-(4, 'Susan ', 'Bagby\r\n', 'diana.bagby@techcenter.net\r\n', '6563453234', '893 Sundance Ln. Richmond, CA 90822\r\n', 'n/a Sole Proprietor', 'n/a', 'n/a', 'n/a', 'n/a'),
-(5, 'Honey\r\n', 'Wayne', 'honey@gmail.com\r\n', '8045902321\r\n', '7238 Shinedown Ct. Richmond, VA 23832\r\n', 'Fort Knight, Co.', 'n/a', 'honey@gmail.com\r\n', '98-902830', '60'),
-(6, 'Lucy', 'Ball', 'lucilleball@gmail.com\r\n', '8552342343\r\n', 'n/a', 'Lexi Cultures Kitchen', '2300 5th St. Louis, MO 63121\r\n', 'admin@cultureskitchen.com', '92-390283', '298'),
-(7, 'Joseph', 'Ferraro', 'Aferraro81@gmail.com', '8045178731', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a'),
-(8, 'Ron', 'Wade', 'ron.wade@hotmail.com', '256567345', '342 Riverside Dr. Lakeview, MN 34230', 'Ralph Blender Co.', '2344535645', 'blender@ralphblenderco.net', '93-9230290', '34242'),
+(4, 'Susan ', 'Bassy\r\n', 'diana.bagby@tech.net\r\n', '6563453234', '893 Sundance Ln. Richmond, CA 90822\r\n', 'n/a Sole Proprietor', 'n/a', 'n/a', 'n/a', 'n/a'),
+(5, 'Honey\r\n', 'Wayne', 'honey@gmeil.com\r\n', '8045902321\r\n', '7238 Shinedown Ct. Richmond, VA 23832\r\n', 'Fort Knight, Co.', 'n/a', 'honey@gmail.com\r\n', '98-902830', '60'),
+(6, 'Lucy', 'Ball', 'lucilleball@gmeil.com\r\n', '8552342343\r\n', 'n/a', 'Lexi Cultures Kitchen', '2300 5th St. Louis, MO 63121\r\n', 'admin@cultureskitchen.com', '92-390283', '298'),
+(7, 'Joe', 'Rodriguez', 'fedorawearer@gmeil.com', '234897298934', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a'),
+(8, 'Ron', 'Wade', 'ron.wade@hotmeil.com', '256567345', '342 Riverside Dr. Lakeview, MN 34230', 'Ralph Blender Co.', '2344535645', 'blender@ralphblenderco.net', '93-9230290', '34242'),
 (9, 'Vanna', 'White', 'white.vanna@wheeloffortune.org', 'n/a', '299 Third St New York, NY 02399', '', '', '', '', ''),
-(10, 'Angelina', 'Jolie', 'jolie@gmail.com', '9293929399', '293 Folklore Ln. Los Angeles, CA 92393', '', '', '', '', ''),
+(10, 'Angelina', 'Jolie', 'jolie@gmeil.com', '9293929399', '293 Folklore Ln. Los Angeles, CA 92393', '', '', '', '', ''),
 (11, 'Louis', 'Armstrong', 'louis.armstrong@yahoo.com', '9328920289', '22 Louisville Dr. Louisville, KY 29300', 'Louisville Productions, Inc.', '9834038948', 'administrator@louisvilleprod.com', '92-29342800', '2000'),
-(12, 'Ben', 'Franklin', 'franklin.ben@aol.com', '8883939030', '200 White House Dr. Penicola, PA 03903', 'Ben Franklin House', '9083039838', 'ben.franklin@bennyhunna.com', '98-392399', '1000'),
-(13, 'John', 'Rogers', 'rogers.stjohn@gmail.com', '9838239239', '3939 Westwood Dr. Los Angeles, CA 90302', '', '', '', '', ''),
-(14, 'Pocahontas', 'Park', 'pocahontas@yahoo.com', '92928338939', '903 Pocohontas Pwy. South Memphis, TN 39293', '', '', '', '', ''),
+(12, 'Ben', 'Franklin', 'franklin.ben@a2l.com', '8883939030', '200 White House Dr. Penicola, PA 03903', 'Ben Franklin House', '9083039838', 'ben.franklin@bennyhunna.com', '98-392399', '1000'),
+(13, 'John', 'Rogers', 'rogers.stjohn@gmeil.com', '9838239239', '3939 Westwood Dr. Los Angeles, CA 90302', '', '', '', '', ''),
+(14, 'Pocahontas', 'Park', 'pocahontas@yahee.li', '92928338939', '903 Pocohontas Pwy. Memphis, TN 39293', '', '', '', '', ''),
 (15, 'Larry', 'Henry', 'larry.henry', '8973493843', '2435 Beverly Hills Blvd. ', '', '', '', '', '');
 COMMIT;
 
